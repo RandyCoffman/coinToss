9bc6478211b1d4446f7263b3c319a9c8b1ab7525
Randy Coffman <sc.coffman45@ymail.com>
2019-10-24 21:32:57 -0400

    first commit

---
+++ coinToss/FlipCoin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace coinToss
+{
+    class FlipCoin
+    {
+        private static readonly Random rnd = new Random();
+        private int headsCount = 0;
+        private int tailsCount = 0;
+
+        public void Toss(int flips)
+        {
+            for (int i = 0; i < flips; i++) {
+                if (rnd.NextDouble() >= 0.5) {
+                    headsCount++;
+                } else {
+                    tailsCount++;
+                }
+            }
+            Console.WriteLine("We fliped the coin {0} times." +
+                " We flipped it on Head {1} times and we flipped it on Tails {2} times.", flips, headsCount, tailsCount );
+        }
+    }
+}
---
+++ coinToss/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace coinToss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string redo;
+            string checkSum;
+            FlipCoin coin = new FlipCoin();
+            Console.WriteLine("Welcome to my coin flipping game!");
+            Console.WriteLine("How many times would you like the coin to flip?");
+            checkSum = Console.ReadLine();
+
+            int flips;
+            while (int.TryParse(checkSum, out flips) == false)
+            {
+                Console.WriteLine("Enter a valid number. No letters and decimal places are allowed.");
+                checkSum = Console.ReadLine();
+            }
+            flips = Convert.ToInt32(checkSum);
+            coin.Toss(flips);
+            Console.WriteLine("Do you want to try again?");
+            redo = Console.ReadLine();
+            switch (redo.Trim().ToLower()) {
+                case "y":
+                case "yes":
+                    Main(null);
+                    break;
+                default:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+    }
+}
